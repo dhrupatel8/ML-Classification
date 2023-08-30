5b5f1e81ad890fec820682b39d0edb6a72a2a99d
Dhruv Patel <dhru.patel8@gmail.com>
2023-08-30 09:55:10 -0700

    first_commit

---
+++ CustomerChurn/main.py
@@ -0,0 +1,67 @@
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+I believe that the situation is the problem and not Srushti
+I believe there is some solution that lies that we are not able to get it because of communication issues.
+We both are strongly agreeing with our inner mind and not noticing each other’s action and efforts.
+We both seems to be frustrated with this relationship
+I cannot provide what she is looking for
+She cannot say what she wants clearly. Also, she should say what she doesn’t want
+She wanted me to be available to her all the time. When she calls me, I should not put my phone on silent.
+She tries to hurt herself by consuming chalk and threaten herself because she thinks she is the problem
+I am stressed because I am failing in all the aspects of my life.
+I am also negative at times.
+Everytime I try to recover, she brings up the last topic of the fight. 
+I cry as a man every 2 days in a week. It is painful
+I feel mental torture
+Today after the call with my parents, her mood was good, so I wanted to do something for both of us. I wanted to play old songs and it will also give me more relief. I asked her, “Can I play song for you”, she got defensive and started bringing the old fight topics.
+I don’t want to understand what’s going on in her mind. I want to understand what should I DO. What exactly should I DO so that it will create peace between us.
+I don’t want to lose myself and be quiet because that will give a strong message to her about my hatred towards her. I do not hate her. I love her. I genuinely love her.
+My mind can only handle some things. My biggest weakness is that I change my mind a lot, and I don’t know what to do career wise, Should I go to Delaware and be a weak person. Or, should I stay here and thrive
+There are somethings in my mind that causing me to go to do drugs.
+Sometimes I think problem is ME and Srushti is suffering because of me
+Why should I waste someone else’s life when its not their fault. 
+Biggest thing for me is Srushti and I feel like a failure because I do anything or not do anything and that will result into a fight. 
+I think I am so low category. Not disciplined to do anything
+I am dependent on her. I want to see her smile
+I want to see her happy
+I want to say I love you every time, and I mean it
+I want to kiss her. When i close my eyes and kiss her. From somewhere I get this positive energy. A sense of calmness
+I don’t understand why she is negative. What is causing her to make the statement that I want distance? My behavior
+May be sometimes I don’t pick up her phone
+Am I doing this intentionally and everytime?
+What triggers me the most is that when she try to hurt herself
+I think that is because I gave her my heart and I really care about her.
+I really don’t know what action to take. I ask her and she says “Don’t worry, I will make distance”
+She is my fiance and she talks about not coming back to my life
+I believe she is my wife and she wants to die.
+She also says that she will le6ave me when I have everything
+I told her multiple times that when I hug her, I feel like I have everything, but she cannot recall that when she talk about leaving me.
+I don’t know everything. I am equal to a common man who is making observations and acting accordingly. I just want to act
+My state of mind is not at peace. Headache, Stress of Job, Stress of Loan, Stress of being a burden on someone. My mind is not performing the best.
+She smiles at me with not her willingness.
+She got blade out today to cut herself.  I was feeling dizzy in the morning. I really needed her help today. I was feeling depressed today when I talked to my parents.
+She said I insulted her in front of my parents. Why she cannot say “Hey Dhruv, can I tell you one thing, I did not like when you said …… to me. I felt it this way. Can you not do this again please, it does affect my self respect  or whatever it is” I swear to god, the above statement is actionable (I can take action on - Do Not or Do something because I felt this different) In the next encounter of this problem, my brain will recall this incident, and I will stop it completely
+This being said, the communication part is missing here where I am communicating and I feel like Srushti is partially communicating.
+I told her to write on this document and she did not follow it. I told her to turn the camera to her, she did not follow it. She believe she is doing everything I ask for. 
+Before she use to care about the stress I was having and now she cares less about me.
+She don’t ask me what I ate anymore
+
+
+
